99a8de0517003c1eb369e107fc4bf289473dcc8a
Manik Shrivastav <manikshrivastav163@gmail.com>
2022-10-14 14:40:26 +0545

    Ready For Launch 1

--- app/src/main/java/com/dgo/dgo/fragments/HomeFragment.java
+++ app/src/main/java/com/dgo/dgo/fragments/HomeFragment.java
@@ -28,12 +28,16 @@ import com.dgo.dgo.modules.NewProductsAdapter;
 import com.dgo.dgo.modules.NewProductsModel;
 import com.dgo.dgo.modules.PopularProductsAdapter;
 import com.dgo.dgo.modules.PopularProductsModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -109,22 +113,29 @@ public class HomeFragment extends Fragment {
         ImageSlider imageSlider= root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
 
-        FirebaseDatabase.getInstance().getReference().child("Slider")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (DataSnapshot data: snapshot.getChildren()){
-                            slideModels.add(new SlideModel(data.child("url").getValue().toString(),data.child("Title").getValue().toString(),ScaleTypes.FIT));
-                        }
+        db.collection("ImageSlider").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+
+                    for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+
+                        slideModels.add(new SlideModel(queryDocumentSnapshot.getString("url"),ScaleTypes.FIT));
                         imageSlider.setImageList(slideModels,ScaleTypes.FIT);
                     }
+                }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Toast.makeText (getActivity (), "Error in Loading!", Toast.LENGTH_SHORT).show ();
-                    }
-                });
-
+                else{
+                    Toast.makeText (getActivity (), "Error in Loading!", Toast.LENGTH_SHORT).show ();
+                }
+            }
+        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText (getActivity (), "Error in Loading!", Toast.LENGTH_SHORT).show ();
+                            }
+                        });
 
 
 
